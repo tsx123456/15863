0c4abcd9b69405fd67ddff016035c22738d1d642
上帝的梦 <1597691532@qq.com>
2025-12-11 17:11:42 +0800

    1

--- index.0f247.js
+++ index.0f247.js
@@ -1,4 +1,4 @@
-System.register(["./application.56d8a.js"], function (_export, _context) {
+System.register(["./application.bbd4e.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
